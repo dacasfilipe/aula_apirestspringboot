4da5eacd821ed08ef2996cb8e72f3c8f4d9c52b8
dacasfilipe <filiperdacas@gmail.com>
2024-03-27 13:41:04 -0300

    Initial commit

---
+++ src/main/java/com/apirestspringboot/ApirestspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.apirestspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestspringbootApplication.class, args);
+	}
+
+}
